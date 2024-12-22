e728c11051e1fe63740990caaf84bbff906a77a7
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-12-22 11:07:36 -0300

    style: format URL strings for better readability in configuration files

--- src/scraper/visualcrossing_api/config.py
+++ src/scraper/visualcrossing_api/config.py
@@ -1,8 +1,15 @@
 TEMPLATE_URL_API = (
-    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
-    '{location}/{start_date}/{final_date}?key={WEATHER_API_KEY}&unitGroup=metric&include=days&contentType=csv'
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/'
+    'services/timeline/{location}/{start_date}/{final_date}?key={WEATHER_API_KEY}'
+    '&unitGroup=metric&include=days&contentType=csv'
 )
 
-ICON_URL = 'https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/{icon}.svg'
+ICON_URL = (
+    'https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/'
+    '1st%20Set%20-%20Color/{icon}.svg'
+)
 
-TEMPLATE_ICON_TAG = '<img src="https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/{icon}.svg" alt="{icon}">'
+TEMPLATE_ICON_TAG = (
+    '<img src="https://raw.githubusercontent.com/visualcrossing/WeatherIcons/'
+    'main/SVG/1st%20Set%20-%20Color/{icon}.svg" alt="{icon}">'
+)
