829a086b31545ad8fe8d5379cbac5238c12cb4e8
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-12-27 18:02:01 -0300

    feat: export locations_api and visualcrossing_api functions for convenience

---
+++ src/scraper/locations_api/__init__.py
@@ -0,0 +1,3 @@
+from src.scraper.locations_api.main import get_locations
+
+__all__ = ['get_locations']
---
+++ src/scraper/visualcrossing_api/__init__.py
@@ -0,0 +1,3 @@
+from src.scraper.visualcrossing_api.main import get_weather_data
+
+__all__ = ['get_weather_data']
